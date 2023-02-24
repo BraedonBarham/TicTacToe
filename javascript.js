8734bd47d09b0a697fd17cd741fe6c09ad4fd315
BraedonBarham <bbraedon10@gmail.com~>
2023-02-24 00:08:41 -0700

    Winner Logic Completed

--- javascript.js
+++ javascript.js
@@ -16,31 +16,95 @@ var gameBoard = (() => {
         divElement = document.getElementById(divID)
         playerChoice = document.createTextNode(player)
         divElement.appendChild(playerChoice)
-
+        console.log(gameBoard.board[divID] + " lol")
+        gameLogic.checkColumn()
+        gameLogic.checkRow()
+        gameLogic.checkDiagonals()
     }
-
 
     return {board, updateBoard};
 })();
 
 var gameLogic = (() => {
-    const checkRow = () => {
-        if (board[0][0] === board[0][1] === board[0][2] === "X") return true;
-        if (board[1][0] === board[1][1] === board[1][2] === "X") return true;
-        if (board[2][0] === board[2][1] === board[2][2] === "X") return true;
-
-        if (board[0][0] === board[0][1] === board[0][2] === "O") return true;
-        if (board[1][0] === board[1][1] === board[1][2] === "O") return true;
-        if (board[2][0] === board[2][1] === board[2][2] === "O") return true;
-        return false;
-    }
     const checkColumn = () => {
-        if (board[0][0] === board[1][0] === board[2][0]) return true;
-        if (board[0][1] === board[1][1] === board[1][2]) return true;
-        if (board[0][2] === board[1][2] === board[2][2]) return true;
-        return false;
+        // Check first column X
+        if (gameBoard.board["div1"] === 'X' && gameBoard.board["div4"] === 'X'  && gameBoard.board["div7"] === 'X'){
+            console.log("X wins")
+        }
+        // Check second column X
+        if (gameBoard.board["div2"] === 'X' && gameBoard.board["div5"] === 'X'  && gameBoard.board["div8"] === 'X'){
+            console.log("X wins")
+          
+        }
+        // Check third column X
+        if (gameBoard.board["div3"] === 'X' && gameBoard.board["div6"] === 'X'  && gameBoard.board["div9"] === 'X'){
+            console.log("X wins")
+        }
+
+         // Check first column O
+         if (gameBoard.board["div1"] === 'O' && gameBoard.board["div4"] === 'O'  && gameBoard.board["div7"] === 'O'){
+            console.log("O wins")
+        }
+        // Check second column O
+        if (gameBoard.board["div2"] === 'O' && gameBoard.board["div5"] === 'O'  && gameBoard.board["div8"] === 'O'){
+            console.log("O wins")
+          
+        }
+        // Check third column O
+        if (gameBoard.board["div3"] === 'O' && gameBoard.board["div6"] === 'O'  && gameBoard.board["div9"] === 'O'){
+            console.log("O wins")
+        }
+    }
+    const checkRow = () => {
+        // Check first row X
+        if (gameBoard.board["div1"] === 'X' && gameBoard.board["div2"] === 'X'  && gameBoard.board["div3"] === 'X'){
+            console.log("X wins")
+        }
+        // Check second row X
+        if (gameBoard.board["div4"] === 'X' && gameBoard.board["div5"] === 'X'  && gameBoard.board["div6"] === 'X'){
+            console.log("X wins")
+          
+        }
+        // Check third row X
+        if (gameBoard.board["div7"] === 'X' && gameBoard.board["div8"] === 'X'  && gameBoard.board["div9"] === 'X'){
+            console.log("X wins")
+        }
+
+        // Check first row O
+        if (gameBoard.board["div1"] === 'O' && gameBoard.board["div2"] === 'O'  && gameBoard.board["div3"] === 'O'){
+            console.log("O wins")
+        }
+        // Check second row O
+        if (gameBoard.board["div4"] === 'O' && gameBoard.board["div5"] === 'O'  && gameBoard.board["div6"] === 'O'){
+            console.log("O wins")
+        
+        }
+        // Check third row O
+        if (gameBoard.board["div7"] === 'O' && gameBoard.board["div8"] === 'O'  && gameBoard.board["div9"] === 'O'){
+            console.log("O wins")
+        }
     }
 
+    const checkDiagonals = () => {
+        // Check top left to bottom right X diagonal
+        if (gameBoard.board["div1"] === 'X' && gameBoard.board["div5"] === 'X'  && gameBoard.board["div9"] === 'X'){
+            console.log("X wins")
+        }
+        // Check bottom left to top right X diagonal
+        if (gameBoard.board["div7"] === 'X' && gameBoard.board["div5"] === 'X'  && gameBoard.board["div6"] === 'X'){
+            console.log("X wins")
+        }
+        // Check top left to bottom right O diagonal
+        if (gameBoard.board["div1"] === 'O' && gameBoard.board["div5"] === 'O'  && gameBoard.board["div9"] === 'O'){
+            console.log("O wins")
+        }
+        // Check bottom left to top right O diagonal
+        if (gameBoard.board["div7"] === 'O' && gameBoard.board["div5"] === 'O'  && gameBoard.board["div6"] === 'O'){
+            console.log("O wins")
+        }
+    }
+    
+    return {checkColumn, checkRow, checkDiagonals}
 
 })();
 
@@ -48,7 +112,6 @@ const divButtons = (() => {
     var divs = Array.from(document.getElementsByClassName("board-div"));
     const assignHandlers = () =>{
         divs.forEach((div) => div.addEventListener("click", function() {
-        spotCheck.checkSpot(div);
         spotCheck.addUserPick(div);
          }));
     };
@@ -56,33 +119,28 @@ const divButtons = (() => {
 
 })()
 const spotCheck = (() =>{
-    const checkSpot = div => {
-        console.log(gameBoard.board)
-        if (gameBoard.board[div.id] === ""){
-            console.log("Empty space")
-        }
-
-    }
     const addUserPick = (div) => {
         if (gameBoard.board[div.id] !== ""){
             console.log("Spot already filled with " + gameBoard.board[div.id]);
         }
         else if (gameBoard.board[div.id] === ""){
-            if (currentPlayer.getTurn() === 'X'){
-                gameBoard.updateBoard(div.id, 'X')
+            if (currentPlayer.getTurn() === "X"){
+                gameBoard.updateBoard(div.id, "X")
                 currentPlayer.changeTurn()
                 displayControl.displayBoard();
+                
+
 
             }
-            else if (currentPlayer.getTurn() === 'O'){
-                gameBoard.updateBoard(div.id, 'O')
+            else if (currentPlayer.getTurn() === "O"){
+                gameBoard.updateBoard(div.id, "O")
                 currentPlayer.changeTurn()
                 displayControl.displayBoard()
             }
         }
     }
 
-    return {checkSpot, addUserPick};
+    return {addUserPick};
 
 })();
 
@@ -98,13 +156,13 @@ const displayControl = (() => {
 const playerLogic = (startingPlayer) => {
     var turn = startingPlayer
     const changeTurn = () => {
-        if (turn === 'X'){
-            turn = 'O'
-            return 'O'
+        if (turn === "X"){
+            turn = "O"
+            return "O"
         }
         else {
-            turn = 'X'
-            return 'X'
+            turn = "X"
+            return "X"
         }
     }
     const getTurn = () => turn;
@@ -117,7 +175,7 @@ return {changeTurn, getTurn}
 var gameLoop = (() => {
     var divLogic = divButtons;
     divLogic.assignHandlers();
-    currentPlayer = playerLogic('X')
+    currentPlayer = playerLogic("X")
     return;
 
 })();
